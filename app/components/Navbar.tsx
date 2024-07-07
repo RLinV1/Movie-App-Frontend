db85fe7cad7673c6f1748c4baa5add80e198b644
Raymond Lin <86033049+ZaRamen@users.noreply.github.com>
2024-07-07 17:22:39 -0400

    switched navbar link

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ import { MdMovie } from "react-icons/md";
 export function NavbarReact() {
   return (
     <Navbar fluid rounded className="bg-slate-800">
-      <Navbar.Brand as={Link} href="http://localhost:3000">
+      <Navbar.Brand as={Link} href="https://movie-app-frontend-eta.vercel.app">
         <MdMovie className="w-10 h-10 mx-2" />
         <span className="self-center whitespace-nowrap text-xl font-bold dark:text-white">Movie App</span>
       </Navbar.Brand>
